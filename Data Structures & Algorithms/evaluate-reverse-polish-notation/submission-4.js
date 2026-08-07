144b92db34b1e52ed7801e358676852c18002983
akash9817 <45507493+akash9817@users.noreply.github.com>
2026-08-07 12:27:03 +0530

    Add: evaluate-reverse-polish-notation - submission-4

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-4.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+              let stack = [];
+  
+      for (let c of tokens) {
+          if (c === "+") {
+              stack.push(stack.pop() + stack.pop());
+          } else if (c === "-") {
+              let second = stack.pop();
+              let first = stack.pop();
+              stack.push(first - second);
+          } else if (c === "*") {
+              stack.push(stack.pop() * stack.pop());
+          } else if (c === "/") {
+              let second = stack.pop();
+              let first = stack.pop();
+              stack.push(parseInt(first / second));
+          } else {
+              stack.push(parseInt(c));
+          }
+      }
+  
+      return stack[0];  
+    }
+}
